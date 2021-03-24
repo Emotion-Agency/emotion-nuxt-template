0a3f053e65efcdaae537eda7957e6f5ca62ab3b8
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2021-03-24 19:52:40 +0200

    init

---
+++ gulp/tasks/images.js
@@ -0,0 +1,29 @@
+const { src, dest } = require('gulp')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+
+const config = require('../config')
+
+function images() {
+  return src(config.src.img)
+    .pipe(
+      webp({
+        quality: 75,
+      })
+    )
+    .pipe(dest(config.build.img))
+    .pipe(src(config.src.img))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest(config.build.img))
+}
+
+module.exports = images
