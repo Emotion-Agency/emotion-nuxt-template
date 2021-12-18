54688c16db32ce866f6b1eb9291d2c52b662bbd3
letmewin22 <lkosteckiy5@gmail.com>
2021-12-18 21:20:15 +0200

    update mixins and hello

--- scripts/utils/hello.js
+++ scripts/utils/hello.js
@@ -1,17 +1,15 @@
 export const hello = () => {
+  const text = '⚒ by Emotion Agency'
   if (navigator.userAgent.toLowerCase().includes('chrome')) {
     const args = [
-      '\n %c ⚒ by emotiON: %c https://emotion-agency.com %c %c \n\n',
-      'color: #fff; background: #a03adb; padding:5px 0;',
-      'color: #fff; background: #ffffff; padding:5px 0;',
-      'background: #fff; padding:5px 0;',
-      'color: #b0976d; background: #fff; padding:5px 0;',
+      `\n %c ${text} \n`,
+      'color: #fff; background: #9047ff; padding:5px 0;',
     ]
     window.console.log.apply(console, args)
     return
   }
 
   if (window.console) {
-    window.console.log('Made by emotiON: https://emotion-agency.com')
+    window.console.log(`${text}: https://emotion-agency.com/en`)
   }
 }
